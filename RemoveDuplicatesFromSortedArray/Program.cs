79cc1e62fa98b8406cac0d6deadb7770aea7bd3a
YuiHsu <54923318+YuiiHsu@users.noreply.github.com>
2020-05-22 11:43:47 +0800

    新增註解

--- RemoveDuplicatesFromSortedArray/Program.cs
+++ RemoveDuplicatesFromSortedArray/Program.cs
@@ -1,14 +1,24 @@
 ﻿namespace RemoveDuplicatesFromSortedArray
 {
+    /// <summary>
+    /// Program
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// 26. Remove Duplicates from Sorted Array
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
         public int RemoveDuplicates(int[] nums)
         {
+            // 如果input參數 nums是空陣列，則回長度 = 0
             if (nums.Length == 0)
             {
                 return 0;
             }
 
+            // 因為陣列裡第一個數字[0]前面沒有可以比較的數,所以count跟i直接從1開始
             var count = 1;
             for (int i = 1; i < nums.Length; i++)
             {
